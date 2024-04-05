6a7c90520bfd438d0187449f2cad9b7e062058d0
Artem Volovyi <artem.volovyi@nure.ua>
2024-04-05 19:06:02 +0300

    Add base server setup

--- Task2/AutoCab.Server/Program.cs
+++ Task2/AutoCab.Server/Program.cs
@@ -4,8 +4,24 @@ using AutoCab.Server.BuildInjections;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSetSwagger();
+builder.Services.AddMediatR(configuration =>
+    configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddLogging();
+builder.Services.AddCors();
 builder.Services.AddDbSetup(builder.Configuration);
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseCors(CorsInjection.PolicyName);
+
+app.MapControllers();
 
 app.Run();
