f2fd97e3cb508821902feb91bf70ffebb178044f
Artem Volovyi <artem.volovyi@nure.ua>
2024-05-01 21:10:13 +0300

    Add sign in and sign up pages

--- Task4/autocab.client/src/App.tsx
+++ Task4/autocab.client/src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
